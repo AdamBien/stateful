ee4083a5d7c1b5da44353f6f83e8d16c13ae5372
Adam Bien <abien@adam-bien.com>
2015-06-29 12:40:09 +0200

    Query for state ids implemented

--- statefulapp/src/main/java/com/airhacks/stateful/machine/entity/StateMachine.java
+++ statefulapp/src/main/java/com/airhacks/stateful/machine/entity/StateMachine.java
@@ -3,6 +3,7 @@ package com.airhacks.stateful.machine.entity;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Lob;
+import javax.persistence.NamedQuery;
 import org.apache.commons.scxml2.SCInstance;
 import org.apache.commons.scxml2.SCXMLExecutor;
 
@@ -11,10 +12,14 @@ import org.apache.commons.scxml2.SCXMLExecutor;
  * @author airhacks.com
  */
 @Entity
+@NamedQuery(name = StateMachine.findAllNames, query = "SELECT s.stateMachineId FROM StateMachine s")
 public class StateMachine {
 
     @Id
     private String stateMachineId;
+
+    private static final String PREFIX = "com.airhacks.stateful.machine.entity.StateMachine.";
+    public final static String findAllNames = ".findAllNames";
 
     @Lob
     private SCInstance state;
--- statefulapp/src/test/java/com/airhacks/stateful/machine/entity/StateMachineIT.java
+++ statefulapp/src/test/java/com/airhacks/stateful/machine/entity/StateMachineIT.java
@@ -1,6 +1,7 @@
 package com.airhacks.stateful.machine.entity;
 
 import java.io.IOException;
+import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
@@ -11,7 +12,9 @@ import org.apache.commons.scxml2.io.SCXMLReader;
 import org.apache.commons.scxml2.model.ModelException;
 import org.apache.commons.scxml2.model.SCXML;
 import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -49,6 +52,26 @@ public class StateMachineIT {
 
         SCXML actualState = actual.getStateMachine();
         assertThat(initialState.getName(), is(actualState.getName()));
+    }
+
+    @Test
+    public void findAllNames() throws Exception {
+        String id = "machine-" + System.currentTimeMillis();
+        SCXML initialState = SCXMLReader.read(this.getClass().getResourceAsStream("/state.xml"));
+        SCXMLExecutor initial = new SCXMLExecutor();
+        initial.setStateMachine(initialState);
+        initial.go();
+
+        StateMachine stateMachine = new StateMachine(id, initial);
+        this.tx.begin();
+        this.em.merge(stateMachine);
+        this.tx.commit();
+
+        List<String> resultList = this.em.createNamedQuery(StateMachine.findAllNames, String.class).
+                getResultList();
+        assertNotNull(resultList);
+        assertTrue(resultList.contains(id));
+
     }
 
 }
