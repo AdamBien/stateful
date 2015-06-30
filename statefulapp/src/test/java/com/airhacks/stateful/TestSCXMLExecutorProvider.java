c1c3ebb2ace2d0378c2becdf22997e52e8333cbf
Adam Bien <abien@adam-bien.com>
2015-06-30 09:00:02 +0200

    JS evaluation added

--- statefulapp/src/test/java/com/airhacks/stateful/TestSCXMLExecutorProvider.java
+++ statefulapp/src/test/java/com/airhacks/stateful/TestSCXMLExecutorProvider.java
@@ -1,6 +1,8 @@
 package com.airhacks.stateful;
 
 import org.apache.commons.scxml2.SCXMLExecutor;
+import org.apache.commons.scxml2.env.javascript.JSContext;
+import org.apache.commons.scxml2.env.javascript.JSEvaluator;
 import org.apache.commons.scxml2.io.SCXMLReader;
 import org.apache.commons.scxml2.model.SCXML;
 
@@ -13,6 +15,8 @@ public class TestSCXMLExecutorProvider {
     public static SCXMLExecutor create() throws Exception {
         SCXML scxml = SCXMLReader.read(TestSCXMLExecutorProvider.class.getResourceAsStream("/state.xml"));
         SCXMLExecutor executor = new SCXMLExecutor();
+        executor.setRootContext(new JSContext());
+        executor.setEvaluator(new JSEvaluator());
         executor.setStateMachine(scxml);
         executor.go();
         return executor;
