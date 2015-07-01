0f58ed7ca989753fd0c8283fb4c79e79f9a954ea
Adam Bien <abien@adam-bien.com>
2015-07-01 03:18:10 +0200

    Reproducer for not-serializable exception implemented

--- statefulapp/src/test/java/com/airhacks/stateful/machine/boundary/StateManagerTest.java
+++ statefulapp/src/test/java/com/airhacks/stateful/machine/boundary/StateManagerTest.java
@@ -2,9 +2,16 @@ package com.airhacks.stateful.machine.boundary;
 
 import com.airhacks.stateful.TestSCXMLExecutorProvider;
 import com.airhacks.stateful.machine.control.DefinitionStore;
+import com.airhacks.stateful.machine.control.SCXMLExecutorFactory;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import javax.json.Json;
 import javax.json.JsonArray;
 import javax.json.JsonObject;
+import org.apache.commons.scxml2.SCInstance;
 import org.apache.commons.scxml2.SCXMLExecutor;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
@@ -54,4 +61,35 @@ public class StateManagerTest {
         assertThat(hackerState, is("isolated"));
     }
 
+    //reproducer
+    @Test
+    public void triggerWithParametersWithDetachment() throws Exception {
+        String stateMachineId = "test";
+        SCXMLExecutor executor = TestSCXMLExecutorProvider.create();
+        SCInstance detachInstance = executor.detachInstance();
+        SCInstance reserialized = reserialize(detachInstance);
+        SCXMLExecutor copy = SCXMLExecutorFactory.create(reserialized);
+
+        when(this.ds.find(stateMachineId)).thenReturn(copy);
+        JsonObject condition = Json.createObjectBuilder().add("user", "hacker").build();
+        String event = "login";
+        JsonObject result = this.cut.trigger(stateMachineId, event, condition);
+        //serialization after state change fails
+        reserialize(reserialized);
+        System.out.println("result = " + result);
+        JsonArray results = result.getJsonArray("current-state");
+        String hackerState = results.getString(0);
+        assertThat(hackerState, is("isolated"));
+    }
+
+    SCInstance reserialize(SCInstance instance) throws IOException, ClassNotFoundException {
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        ObjectOutputStream oos = new ObjectOutputStream(stream);
+        oos.writeObject(instance);
+        oos.flush();
+        byte[] bytes = stream.toByteArray();
+        ByteArrayInputStream iis = new ByteArrayInputStream(bytes);
+        ObjectInputStream ois = new ObjectInputStream(iis);
+        return (SCInstance) ois.readObject();
+    }
 }
